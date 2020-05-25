5cdf077799262c7c3405ecd6ce6d8432f415e956
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-05-25 09:26:42 +0200

    test: Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import { render } from 'react-native-testing-library';
 it('renders correctly', async () => {
   const { findByText } = render(<App />);
 
-  const learnMore = await findByText('Learn More');
+  const learnMore = await findByText('Hello, React!');
   expect(learnMore).toBeDefined();
 });
