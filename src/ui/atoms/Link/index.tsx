551aedb8ef3462387dfedca98bfa36b5ed71ffd5
Przemysław Żydek <przemyslaw.zydek@gmail.com>
2020-05-28 09:10:30 +0200

    chore: improve user creation form look

---
+++ src/ui/atoms/Link/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useCallback } from 'react';
+import { AProps, A } from '../../../styles/typography';
+import { Linking } from 'react-native';
+
+export interface LinkProps extends AProps {
+  href: string;
+}
+
+const Link: FC<LinkProps> = ({ variant, href, children }) => {
+  const handlePress = useCallback(() => {
+    Linking.openURL(href);
+  }, [href]);
+
+  return (
+    <A onPress={handlePress} variant={variant}>
+      {children}
+    </A>
+  );
+};
+
+export default Link;
--- src/utils/keyboardAvoidingView.ts
+++ src/utils/keyboardAvoidingView.ts
@@ -1,4 +1,4 @@
 import { Platform } from 'react-native';
 
 export const getBehavior = () =>
-  Platform.OS === 'ios' ? 'position' : 'height';
+  Platform.OS === 'ios' ? 'position' : 'padding';
